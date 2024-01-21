08f6444f4ed82c67a6edb850b0a376f39d6053e8
vggog <gluhovkich@gmail.com>
2024-01-22 00:04:27 +0500

    Добавить ручку для создания блюда

--- src/dish/service.py
+++ src/dish/service.py
@@ -1,6 +1,7 @@
 from fastapi import Depends
 
 from .repository import Repository
+from .schemas import CreateDishSchema
 
 
 class Service:
@@ -13,3 +14,9 @@ class Service:
 
     def get_all_dishes(self):
         return self.repository.get_all()
+
+    def create_dish(self, submenu_id: str, created_dish: CreateDishSchema):
+        return self.repository.create(
+            submenu_id=submenu_id,
+            **created_dish.model_dump()
+        )
--- src/dish/views.py
+++ src/dish/views.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from starlette import status
 
 from .service import Service
-from .schemas import DishSchema
+from .schemas import DishSchema, CreateDishSchema
 
 
 dish_router = APIRouter(
@@ -15,7 +15,22 @@ dish_router = APIRouter(
     response_model=list[DishSchema],
     status_code=status.HTTP_200_OK
 )
-def get_all_submenus(
+def get_all_dishes(
         service=Depends(Service),
 ):
     return service.get_all_dishes()
+
+
+@dish_router.post(
+    "/",
+    response_model=DishSchema,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_dish(
+        submenu_id: str,
+        created_dish: CreateDishSchema,
+        service=Depends(Service),
+):
+    return service.create_dish(
+        submenu_id=submenu_id, created_dish=created_dish
+    )
