b87e0f28e77ecc540d8bb7ec6ce18dfe3ce2b06a
vggog <gluhovkich@gmail.com>
2024-02-13 17:55:41 +0500

    Добавить celery-beat

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
 from src.factory import AppFactory
-from src.tasks.tasks import update_base
 
 app = AppFactory.create_app()
-
-
-@app.on_event('startup')
-def start_up_project():
-    update_base.delay()
--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from celery import Celery
 from openpyxl import load_workbook
 
@@ -11,11 +13,12 @@ celery = Celery(
 )
 
 
-@celery.task(
-    default_retry_delay=15,
-)
+@celery.task
 def update_base():
     """Задача обновления данных в базе из файла."""
     parser = ParseExcel(load_workbook(app_config.excel_doc_path).worksheets[0])
     updater = DataBaseUpdater(parser.parse())
-    updater.run()
+    asyncio.run(updater.run())
+
+
+celery.add_periodic_task(15, update_base, name='db_updater')
